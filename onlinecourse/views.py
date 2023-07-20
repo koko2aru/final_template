b4c0a8041029d295c070504e5d976e2bea3b3417
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-20 13:23:43 +0200

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -117,7 +117,7 @@ def submit(request, course_id):
     Submission.objects.create(enrollment=enrollment)
     choices = extract_answers()
     Submissions.append(choices=choices)
-    return redirect('onlinecourse:show_exam_result' submission_id)
+    return redirect('onlinecourse:show_exam_result' arg=(submission_id))
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
