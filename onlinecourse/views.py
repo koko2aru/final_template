c2aaa7a69ba4d14ff6b5f8f19f851c5e86f36dd5
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-20 12:45:03 +0200

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -110,18 +110,24 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
-
+def submit(request, course_id):
+    user = Enrollment.objects.get(user=request.user)
+    course = Enrollment.objects.get(course=couse_id)
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    Submission.objects.create(enrollment=enrollment)
+    choices = extract_answers()
+    choices.append(Submissions, choices)  
+    return redirect('onlinecourse:show_exam_result' submission_id)
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -130,7 +136,9 @@ def enroll(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    submission_id = request.submission_id
+    course_id = request.course_id
 
 
 
